62a18f4f1771f736b4bfc7d71e83e16df875bc25
AdamLee <handsomeadam@126.com>
2023-04-29 21:30:37 +0800

    first commit

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -11,9 +11,9 @@ const ROPSTEN_PRIVATE_KEY = "Your private key";
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
-    }
+    // goerli: {
+    //   url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    //   accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    // }
   }
 };
