da8d8050a528f9a6824b28c60333d4fb1221ad56
AdamLee <handsomeadam@126.com>
2023-05-01 19:48:21 +0800

    add WETH and test tokens

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -5,10 +5,18 @@ async function main() {
   
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
+    // const Token = await ethers.getContractFactory("Token");
+    // const token = await Token.deploy();
+
+    // NFTVault Test Coin
+    const Tokens = await ethers.getContractFactory("NFTVaultTest");
+    const token = await Tokens.deploy();
+
+    const WETH = await ethers.getContractFactory("WETH9");
+    const weth = await WETH.deploy();
   
-    console.log("Token address:", token.address);
+    console.log("NFTVaultTest Token address:", token.address);
+    console.log("WETH address:", weth.address);
   }
   
   main()
